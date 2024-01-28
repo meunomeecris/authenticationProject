fa826472fd11ac7120f0c576f83b6b292290f52e
Cris Messias <meunomeecriss@gmail.com>
2024-01-28 11:08:19 +0100

    Initial Commit

---
+++ AuthenticationProject/AuthenticationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationProjectApp.swift
+//  AuthenticationProject
+//
+//  Created by Cris Messias on 28/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthenticationProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthenticationProject
+//
+//  Created by Cris Messias on 28/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
